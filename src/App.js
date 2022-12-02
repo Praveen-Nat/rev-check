2c67b29006def394860c03b73c82c70505cb61a3
Praveen M <praveen.02@doodleblue.com>
2022-12-02 19:03:04 +0530

    API test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React from "react";
 function App() {
+  const textGearkey = "Basic kDLREYBUZdMxHSV4";
+
+  const checkSpell = (e) => {
+    let textData = e?.target?.value;
+
+//  fetch("https://api.textgears.com/spelling", {
+//       method: "POST", // or 'PUT'
+//       // TODO extract headers and params seperate variables
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: textGearkey,
+//       },
+//       body: JSON.stringify({
+//         text: textData,
+//         language: "en-GB",
+//         ai: "true",
+//       }),
+//     })
+//       .then((response) => response.json())
+//       .then((data) => {
+//         console.log("Success:", data);
+//       })
+//       .catch((error) => {
+//         console.error("Error:", error);
+//       });
+  };
+
   return (
     <div className="app-container">
       <div className="main-card">
-      <textarea className="editor-area" spellCheck="false" onChange={(e)=> {
-        console.log(e.target.value);
-      } } />
+        <textarea
+          className="editor-area"
+          spellCheck="false"
+          onChange={checkSpell}
+        />
       </div>
     </div>
   );
