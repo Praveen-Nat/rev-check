cff94a56cd5c09e31813eb1d2ce5794aab9caa6e
Praveen M <praveen.02@doodleblue.com>
2022-12-02 17:27:28 +0530

    repo creation

--- src/App.js
+++ src/App.js
@@ -3,21 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="main-card">
+      <textarea className="editor-area" spellCheck="false" onChange={(e)=> {
+        console.log(e.target.value);
+      } } />
+      </div>
     </div>
   );
 }
